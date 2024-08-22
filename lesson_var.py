590de8d057d396959983c0eec058764c43af330e
Garrret <perehodcev.igor@yandex.ru>
2024-08-22 15:13:44 +0700

    Add files via upload

---
+++ lesson_var.py
@@ -0,0 +1,5 @@
+kol_complit_task = 12
+kol_chas = 1.5
+course_name = ('Python')
+time_for_task = kol_chas / kol_complit_task
+print('Курс:', course_name, ', всего задач:', kol_complit_task, ', затрачено часов:', kol_chas, ', среднее время выполнения:', time_for_task)
